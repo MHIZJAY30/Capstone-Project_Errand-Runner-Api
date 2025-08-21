e263437648f138848b9bcfe31ca53812300957a3
Juliet Duru <julietduru78@gmail.com>
2025-08-21 11:30:49 -0700

    Registered ErrandItem in admin.py in errand app

--- Errand_Runner/errands/admin.py
+++ Errand_Runner/errands/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import ErrandRequest
+from .models import ErrandRequest, ErrandItem
 
 # Register your models here.
 @admin.register(ErrandRequest)
 class ErrandRequestAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created_at')
     search_fields = ('title', 'description')
+
+@admin.register(ErrandItem)
+class ErrandItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'errand', 'quantity', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'notes')
