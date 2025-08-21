ff5412b8f6e8379d2bc43059c85ab088075243e9
Juliet Duru <julietduru78@gmail.com>
2025-08-21 11:22:52 -0700

    UPdated the installed apps in settings.py with rest_framework_simplejwt

--- Errand_Runner/Errand_Runner/settings.py
+++ Errand_Runner/Errand_Runner/settings.py
@@ -38,18 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'accounts',
     'errands',
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
