6ca71be6cf0cc92fae2fb35cec317c5fb446fbe8
Juliet Duru <julietduru78@gmail.com>
2025-09-01 16:24:53 +0200

    COnfigured databases in settings.py for deployment

--- Errand_Runner/Errand_Runner/settings.py
+++ Errand_Runner/Errand_Runner/settings.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'Errand_Runner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',  
-        'PORT': '5432',
+    'default': 
+dj_database_url.config(
+default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+)
     }
-}
 
 
 # Password validation
@@ -173,5 +173,3 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
